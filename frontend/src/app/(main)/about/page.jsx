a246940857d62fd709d488f233c255918cea2051
Jhanvi-Khare <jhanvikhare26@gmail.com>
2024-04-27 19:22:28 +0530

    signup page

--- frontend/src/app/(main)/about/page.jsx
+++ frontend/src/app/(main)/about/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './About.css'; 
 
-const About = () => {
+function About() {
   return (
-    <div>About</div>
-  )
+    <div className="about-page">
+      <h1>About Us</h1>
+      <p>
+        This is a simple about page for a React application. You can customize
+        this content to provide information about your website, team, or project.
+      </p>
+      <p>
+        Feel free to add more sections like:
+      </p>
+      <ul>
+        <li>Our Mission</li>
+        <li>Our Team</li>
+        <li>Contact Information</li>
+      </ul>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- frontend/src/app/admin/navbar.jsx
+++ frontend/src/app/admin/navbar.jsx
@@ -1,9 +1,77 @@
+import Link from 'next/link';
 import React from 'react'
 
-const AdminNavbar = () => {
-  return (
-    <div>AdminNavbar</div>
-  )
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container">
+                <Link className="navbar-brand fw-bold" href="/">
+                    Next Webapp
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/login">
+                                Login
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/signup">
+                                Signup
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/eventhandling">
+                                Event Handling
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/todolist">
+                                Todo App
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/about">
+                                About Us
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/contactus">
+                                Contact Us
+                            </Link>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button className="btn btn-dark" type="submit">
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+    )
 }
 
-export default AdminNavbar;
+export default Navbar;
