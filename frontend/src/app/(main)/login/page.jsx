c1819c2a9ba5c9ee319dc302837ef02b78de6ff5
Jhanvi-Khare <jhanvikhare26@gmail.com>
2024-04-30 19:34:57 +0530

    pages

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -1,94 +1,84 @@
 import React from 'react'
-<section className="bg-purple-900">
-  <div className="px-0 py-20 mx-auto max-w-7xl sm:px-4">
-    <div className="w-full px-4 pt-5 pb-6 mx-auto mt-8 mb-6 bg-white rounded-none shadow-xl sm:rounded-lg sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 sm:px-6">
-      <h1 className="mb-4 text-lg font-semibold text-left text-gray-900">
-        Log in to your account
-      </h1>
-      <form className="mb-8 space-y-4">
-        <label className="block">
-          <span className="block mb-1 text-xs font-medium text-gray-700">
-            Your Email
-          </span>
-          <input
-            className="form-input"
-            type="email"
-            placeholder="Ex. james@bond.com"
-            inputMode="email"
-            required=""
-          />
-        </label>
-        <label className="block">
-          <span className="block mb-1 text-xs font-medium text-gray-700">
-            Your Password
-          </span>
-          <input
-            className="form-input"
-            type="password"
-            placeholder="••••••••"
-            required=""
-          />
-        </label>
-        <input
-          type="submit"
-          className="w-full py-3 mt-1 btn btn-primary"
-          defaultValue="Login"
-        />
-      </form>
-      <div className="space-y-8">
-        <div
-          className="text-center border-b border-gray-200"
-          style={{ lineHeight: 0 }}
-        >
-          <span
-            className="p-2 text-xs font-semibold tracking-wide text-gray-600 uppercase bg-white"
-            style={{ lineHeight: 0 }}
-          >
-            Or
-          </span>
-        </div>
-        <div className="grid grid-cols-2 gap-4">
-          <a href="#" className="py-3 btn btn-icon btn-google">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="mr-1"
-            >
-              <path d="M20.283,10.356h-8.327v3.451h4.792c-0.446,2.193-2.313,3.453-4.792,3.453c-2.923,0-5.279-2.356-5.279-5.28	c0-2.923,2.356-5.279,5.279-5.279c1.259,0,2.397,0.447,3.29,1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233	c-4.954,0-8.934,3.979-8.934,8.934c0,4.955,3.979,8.934,8.934,8.934c4.467,0,8.529-3.249,8.529-8.934	C20.485,11.453,20.404,10.884,20.283,10.356z" />
-            </svg>
-            <span className="sr-only">Continue with</span> Google
-          </a>
-          <a href="#" className="py-3 btn btn-icon btn-dark">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="mr-1"
-            >
-              <path d="M19.665,16.811c-0.287,0.664-0.627,1.275-1.021,1.837c-0.537,0.767-0.978,1.297-1.316,1.592	c-0.525,0.482-1.089,0.73-1.692,0.744c-0.432,0-0.954-0.123-1.562-0.373c-0.61-0.249-1.17-0.371-1.683-0.371	c-0.537,0-1.113,0.122-1.73,0.371c-0.616,0.25-1.114,0.381-1.495,0.393c-0.577,0.025-1.154-0.229-1.729-0.764	c-0.367-0.32-0.826-0.87-1.377-1.648c-0.59-0.829-1.075-1.794-1.455-2.891c-0.407-1.187-0.611-2.335-0.611-3.447	c0-1.273,0.275-2.372,0.826-3.292c0.434-0.74,1.01-1.323,1.73-1.751C7.271,6.782,8.051,6.563,8.89,6.549	c0.46,0,1.063,0.142,1.81,0.422s1.227,0.422,1.436,0.422c0.158,0,0.689-0.167,1.593-0.498c0.853-0.307,1.573-0.434,2.163-0.384	c1.6,0.129,2.801,0.759,3.6,1.895c-1.43,0.867-2.137,2.08-2.123,3.637c0.012,1.213,0.453,2.222,1.317,3.023	c0.392,0.372,0.829,0.659,1.315,0.863C19.895,16.236,19.783,16.529,19.665,16.811L19.665,16.811z M15.998,2.38	c0,0.95-0.348,1.838-1.039,2.659c-0.836,0.976-1.846,1.541-2.941,1.452c-0.014-0.114-0.021-0.234-0.021-0.36	c0-0.913,0.396-1.889,1.103-2.688c0.352-0.404,0.8-0.741,1.343-1.009c0.542-0.264,1.054-0.41,1.536-0.435	C15.992,2.127,15.998,2.254,15.998,2.38L15.998,2.38z" />
-            </svg>
-            <span className="sr-only">Continue with</span> Apple
-          </a>
-        </div>
-      </div>
-    </div>
-    <p className="mb-4 text-xs text-center text-gray-400">
-      <a href="#" className="text-purple-200 underline hover:text-white">
-        Create an account
-      </a>
-      ·
-      <a href="#" className="text-purple-200 underline hover:text-white">
-        Forgot password
-      </a>
-      ·
-      <a href="#" className="text-purple-200 underline hover:text-white">
-        Privacy &amp; Terms
-      </a>
-    </p>
-  </div>
-</section>
+ 
+  var NewComponent = React.createClass({
+      render: function() {
+        return (
+    
+          <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700">
+            <div className="p-4 sm:p-7">
+              <div className="text-center">
+                <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Sign in</h1>
+                <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
+                  Don't have an account yet?
+                  <a className="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500" href="../examples/html/signup.html">
+                    Sign up here
+                  </a>
+                </p>
+              </div>
+              <div className="mt-5">
+                <button type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
+                  <svg className="w-4 h-auto" width={46} height={47} viewBox="0 0 46 47" fill="none">
+                    <path d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z" fill="#4285F4" />
+                    <path d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z" fill="#34A853" />
+                    <path d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z" fill="#FBBC05" />
+                    <path d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z" fill="#EB4335" />
+                  </svg>
+                  Sign in with Google
+                </button>
+                <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">Or</div>
+                {/* Form */}
+                <form>
+                  <div className="grid gap-y-4">
+                    {/* Form Group */}
+                    <div>
+                      <label htmlFor="email" className="block text-sm mb-2 dark:text-white">Email address</label>
+                      <div className="relative">
+                        <input type="email" id="email" name="email" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="email-error" />
+                        <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                          <svg className="size-5 text-red-500" width={16} height={16} fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                          </svg>
+                        </div>
+                      </div>
+                      <p className="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
+                    </div>
+                    {/* End Form Group */}
+                    {/* Form Group */}
+                    <div>
+                      <div className="flex justify-between items-center">
+                        <label htmlFor="password" className="block text-sm mb-2 dark:text-white">Password</label>
+                        <a className="text-sm text-blue-600 decoration-2 hover:underline font-medium" href="../examples/html/recover-account.html">Forgot password?</a>
+                      </div>
+                      <div className="relative">
+                        <input type="password" id="password" name="password" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="password-error" />
+                        <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                          <svg className="size-5 text-red-500" width={16} height={16} fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                          </svg>
+                        </div>
+                      </div>
+                      <p className="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
+                    </div>
+                    {/* End Form Group */}
+                    {/* Checkbox */}
+                    <div className="flex items-center">
+                      <div className="flex">
+                        <input id="remember-me" name="remember-me" type="checkbox" className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" />
+                      </div>
+                      <div className="ms-3">
+                        <label htmlFor="remember-me" className="text-sm dark:text-white">Remember me</label>
+                      </div>
+                    </div>
+                    {/* End Checkbox */}
+                    <button type="submit" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">Sign in</button>
+                  </div>
+                </form>
+                {/* End Form */}
+              </div>
+            </div>
+          </div>
+        );
+      }
+    });
+
+export default Login
